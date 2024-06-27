374d622b5e11f0f67910ff6993afe13a29895675
msahoo3 <msahoo3@gatech.edu>
2024-06-27 19:00:08 -0400

    framework for map integration

--- src/Budgetize.js
+++ src/Budgetize.js
@@ -3,36 +3,25 @@ import './App.css';
 
 function Budgetize() {
   return (
-    <div className="budgetize">
-    <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center">
-          <img src={logo} className="h-8 w-8 mr-2" alt="logo" />
-          <span className="text-white font-semibold text-xl">Budgetize</span>
+    <div className="budgetize h-screen flex flex-col">
+      <nav className="bg-gray-800 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="flex items-center">
+            <img src={logo} className="h-8 w-8 mr-2" alt="logo" />
+            <span className="text-white font-semibold text-xl">Budgetize</span>
+          </div>
+          <div>
+            {/* Add any additional navbar items here */}
+          </div>
         </div>
-        <div>
-          <a href="#home" className="text-gray-300 hover:text-white mx-2">Home</a>
-          <a href="#about" className="text-gray-300 hover:text-white mx-2">About</a>
-          <a href="#services" className="text-gray-300 hover:text-white mx-2">Services</a>
-          <a href="#contact" className="text-gray-300 hover:text-white mx-2">Contact</a>
+      </nav>
+      <div className="flex-grow">
+        {/* Placeholder for Google Maps/Mapbox integration */}
+        <div className="h-full w-full bg-gray-200 flex items-center justify-center">
+          <p className="text-gray-600">Map will be integrated here</p>
         </div>
       </div>
-    </nav>
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+    </div>
   );
 }
 

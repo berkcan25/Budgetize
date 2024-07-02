7fc8aa27ec26657fc33a921ba74ad61173b7483c
msahoo3 <msahoo3@gatech.edu>
2024-07-01 23:22:41 -0400

    map implementation

--- src/Budgetize.js
+++ src/Budgetize.js
@@ -1,5 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Map, {Marker, Popup} from 'react-map-gl';
+import mapboxgl from "mapbox-gl"
+
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+// mapboxgl.workerClass = require('mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 function Budgetize() {
   return (
@@ -10,15 +16,23 @@ function Budgetize() {
             <img src={logo} className="h-8 w-8 mr-2" alt="logo" />
             <span className="text-white font-semibold text-xl">Budgetize</span>
           </div>
-          <div>
-            {/* Add any additional navbar items here */}
-          </div>
         </div>
       </nav>
       <div className="flex-grow">
-        {/* Placeholder for Google Maps/Mapbox integration */}
         <div className="h-full w-full bg-gray-200 flex items-center justify-center">
-          <p className="text-gray-600">Map will be integrated here</p>
+      <>
+        <Map
+          initialViewState={{
+            longitude: -84.3964,
+            latitude: 33.7749,
+            zoom: 3
+          }}
+          style={{width: '95%', height: '95%'}}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          mapboxAccessToken= {process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        >
+        </Map>
+      </>
         </div>
       </div>
     </div>
